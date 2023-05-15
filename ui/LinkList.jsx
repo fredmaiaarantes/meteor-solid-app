14a74096f672f88c6c253b3ade7e9d9a45619c0a
Frederico Maia <fredmaiaarantes@gmail.com>
2023-05-15 12:28:09 -0300

    Add an empty state. Split the link item into another component.

--- ui/LinkList.jsx
+++ ui/LinkList.jsx
@@ -3,6 +3,43 @@ import { Meteor } from "meteor/meteor";
 import { LinksCollection } from "../api/collection";
 import { Tracker } from "meteor/tracker";
 
+const EmptyState = ({ links }) => (
+  <Show
+    when={links().length === 0}
+  >
+    <div className="gap-x-6 py-5">
+      <div className="min-w-0 flex-auto">
+        <p className="text-sm font-semibold text-gray-900">
+          There are no active links
+        </p>
+        <p className="mt-1 truncate text-xs text-gray-500">
+          Fill out the form above to add a new link.
+        </p>
+      </div>
+    </div>
+  </Show>
+);
+
+const LinkItem = ({ link, onClick }) => (
+  <li className="flex items-center justify-between gap-x-6 py-5">
+    <div className="min-w-0 flex-auto">
+      <p className="text-sm font-semibold text-gray-900">
+        <a href={link.url} target="_blank">{link.title}</a>
+      </p>
+      <p className="mt-1 truncate text-xs text-gray-500">
+        <a href={link.url} target="_blank">{link.url}</a>
+      </p>
+    </div>
+    <button
+      onClick={onClick}
+      type="button"
+      className="rounded-md px-2.5 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+    >
+      Archive
+    </button>
+  </li>
+);
+
 export const LinkList = () => {
   const subscription = Meteor.subscribe("activeLinks");
   const [isReady, setIsReady] = createSignal(false);
@@ -28,33 +65,18 @@ export const LinkList = () => {
   });
 
   return (
-    <ul role="list" className="divide-y divide-gray-100">
-      <Show
-        when={isReady()}
-        fallback={<p className="text-sm">Loading...</p>}
-      >
+    <Show
+      when={isReady()}
+      fallback={<p className="text-sm">Loading...</p>}
+    >
+      <ul role="list" className="divide-y divide-gray-100">
         <For each={links()}>
           {(link) => (
-            <li key={link.url} className="flex items-center justify-between gap-x-6 py-5">
-              <div className="min-w-0 flex-auto">
-                <p className="text-sm font-semibold text-gray-900">
-                  <a href={link.url} target="_blank">{link.title}</a>
-                </p>
-                <p className="mt-1 truncate text-xs text-gray-500">
-                  <a href={link.url} target="_blank">{link.url}</a>
-                </p>
-              </div>
-              <button
-                onClick={() => archiveLink(link)}
-                type="button"
-                className="rounded-md px-2.5 py-1 text-xs font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-              >
-                Archive
-              </button>
-            </li>
+            <LinkItem link={link} onClick={() => archiveLink(link)} />
           )}
         </For>
-      </Show>
-    </ul>
+      </ul>
+      <EmptyState links={links} />
+    </Show>
   );
 }
